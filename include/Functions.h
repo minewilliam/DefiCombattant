06de617605168107b374db641aa42df013059e8e
Gabgaouette <gabgaouette@hotmail.com>
2019-09-27 15:20:46 -0400

    Fonction Turn()
    Création de la fonction Turn()

---
+++ include/Functions.h
@@ -0,0 +1,10 @@
+
+enum {Left, Right};
+
+/************************************************
+* @brief        This function is used to turn the robot
+* @param		uint16_t    : Angle on 360°
+* @param		bool 	    : 0 is Left - 1 is Right
+* @return 		none
+*/
+void Turn(uint16_t Angle, bool Side);
--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,87 @@
 #include <Arduino.h>
 #include <LibRobus.h>
 
-//!Placeholder value
-#define STEPSPERMETER 20000 //Encoder steps over one meter
-#define MOTOR_MINSPEED 0.15
-#define MOTOR_TOPSPEED 0.6
-#define MOTOR_ACCELERATION 1.2
+enum {Left, Right};
+void Turn(uint16_t Angle, bool Side);
 
-//Positions in centimeters
-//X:Left-Right
-//Y:Forward-Back
-int CurrentPosition[] = {0,0};
-int Coordinates[][2] = {{50,0},{50,50},{0,50},{0,0}};
-
-float Speed = 0;
-
-void setup() {
-  BoardInit();
-  delay(2000);
-}
-
-void loop() {
-  for(int i = 0; i < 4; i++)
-  {
-    while(!moveToPosition(Coordinates[i],MOTOR_TOPSPEED));
-  }
-}
-
-bool moveToPosition(int position[2], float topSpeed)
+void setup() 
 {
-  return false;
+  BoardInit();
 }
 
+void loop() 
+{
+  Turn(90, Left);
+  delay(1000);
+  Turn(90, Right);
+  delay(1000);
+}
+
+void Turn(uint16_t Angle, bool Side)
+{
+  bool ExitOne = false;
+  bool ExitTwo = false;
+  uint32_t EncoderCountNeeded = 0;
+  uint32_t EncoderCountLeft = 0;
+  uint32_t EncoderCountRight = 0;
+
+  EncoderCountNeeded = 7822*(uint32_t)Angle/360;
+
+  ENCODER_Reset(Left);
+  ENCODER_Reset(Right);
+
+  if (Side==Left)
+  {
+    MOTOR_SetSpeed(Left,-0.15);
+    MOTOR_SetSpeed(Right,0.15);
+  }
+
+  else
+  {
+    MOTOR_SetSpeed(Left,0.15);
+    MOTOR_SetSpeed(Right,-0.15);
+  }
+
+  while (ExitOne == false || ExitTwo == false)
+  {
+    EncoderCountLeft = ENCODER_Read(Left);
+    EncoderCountRight = ENCODER_Read(Right);
+
+    if (Side == Left)
+    {
+     if (EncoderCountLeft == (0xFFFFFFFF - EncoderCountNeeded))
+      {
+        MOTOR_SetSpeed(Left,0);
+        ExitOne = true;
+      }
+
+      if (EncoderCountRight == EncoderCountNeeded)
+      {
+        MOTOR_SetSpeed(Right,0); 
+        ExitTwo = true;
+      } 
+    }
+    
+    else
+    {
+      if (EncoderCountLeft == EncoderCountNeeded)
+      {
+        MOTOR_SetSpeed(Left,0);
+        ExitOne = true;
+      }
+
+      if (EncoderCountRight == (0xFFFFFFFF - EncoderCountNeeded))
+      {
+        MOTOR_SetSpeed(Right,0); 
+        ExitTwo = true;
+      } 
+    }
+  }
+  ENCODER_Reset(Left);
+  ENCODER_Reset(Right);
+}
+
+void MoveForward (float Speed)
+{
+
+}
